b984a808976a7327b93694c7faad9cc28b7e5ed2
Jarissa12 <105379561+Jarissa12@users.noreply.github.com>
2022-09-12 17:43:20 -0400

    registropersona

---
+++ RegistroOcupaciones-RegistroPersonas/RegistroOcupacion/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroOcupacion.Entidades;
+
+namespace RegistroOcupacion.DAL{
+public class Contexto : DbContext
+{
+    public DbSet<Ocupaciones> Ocupaciones { get; set; }
+
+    public DbSet<Persona> Persona { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+}
+}
+ 
